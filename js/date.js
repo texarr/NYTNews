1579d65031a06159d5e9cec6938ec18261f7c4ff
Daniel Pietrucha <kontakt@kuznia-kodu.com.pl>
2017-07-04 16:06:43 +0200

    switched from months to days

--- js/date.js
+++ js/date.js
@@ -1,43 +1,43 @@
 $(document).ready(function() {
-    var fromMonthVal = 1;
-    var toMonthVal = 1;
+    var fromDayVal = 1;
+    var toDayVal = 1;
     // options from date.js
-    var fromMonth = $('.from-month');
-    for (var i = 1; i <= 12; i++) {
+    var fromDay = $('.from-day');
+    for (var i = 1; i <= 31; i++) {
         var value = $('<option>', {class: "option", value: i}).text(i);
-        fromMonth.append(value);
+        fromDay.append(value);
     }
 
-    var toMonth = $('.to-month');
-    for (var i = 1; i <= 12; i++) {
+    var toDay = $('.to-day');
+    for (var i = 1; i <= 31; i++) {
         var value = $('<option>', {class: "option", value: i}).text(i);
-        if (i === 12) {
+        if (i === 31) {
             value.attr('selected', true);
         }
-        toMonth.append(value);
+        toDay.append(value);
     }
 
-    fromMonth.on('change', function() {
-        // disable toMonth values < this value
+    fromDay.on('change', function() {
+        // disable toDay values < this value
         for (var i = this.value-1; i >= 0; i--) {
-            toMonth["0"].children[i].setAttribute('disabled', true);
+            toDay["0"].children[i].setAttribute('disabled', true);
         }
-        // enable toMonth values > this value
-        for (var i = this.value; i < toMonth["0"].children.length; i++) {
-            $(toMonth["0"].children[i]).attr('disabled', false);
+        // enable toDay values > this value
+        for (var i = this.value; i < toDay["0"].children.length; i++) {
+            $(toDay["0"].children[i]).attr('disabled', false);
         }
-        fromMonthVal = this.value;
+        fromDayVal = this.value;
     });
 
-    toMonth.on('change', function() {
-        // disable fromMonth values > this.value
-        for (var i = this.value; i < fromMonth["0"].children.length; i++) {
-            fromMonth["0"].children[i].setAttribute('disabled', true);
+    toDay.on('change', function() {
+        // disable fromDay values > this.value
+        for (var i = this.value; i < fromDay["0"].children.length; i++) {
+            fromDay["0"].children[i].setAttribute('disabled', true);
         }
-        // enable fromMonth.values < this.value
+        // enable fromDay.values < this.value
         for (var i = 0; i < this.value; i++) {
-            $(fromMonth["0"].children[i]).attr('disabled', false);
+            $(fromDay["0"].children[i]).attr('disabled', false);
         }
-        toMonthVal = this.value;
+        toDayVal = this.value;
     });
 });
